5ade68b24f4824db1c2c366c1cc9319e0e61802e
JaredP872 <jared.proctor7642@gmail.com>
2025-01-04 13:03:50 -0500

    Added comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Declares a constant array containing strings of quotes
 const quotes = [
   "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment. - Buddha",
   "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
@@ -21,22 +22,34 @@ const quotes = [
   "Turn your wounds into wisdom. - Oprah Winfrey",
 ];
 
+// Selects the p element with id (quote-text) so that we can update it dynamicaly
 const quoteText = document.getElementById("quote-text");
+// Selects the button element with the id (quote-btn) so that we can add a click event listener to it
 const quoteBtn = document.getElementById("quote-btn");
 
+// Adds a click event listener to the button.
+// When clicked the button isnide of the arrow function runs
 quoteBtn.addEventListener("click", () => {
-  // Generate random quote
+  // Math.ramdom genartes a random decimal between 0 and 1.
+  // Math.floor rounds down the number to ensure its a valid array index
+  // Math.random() * quotes.length: Scales the random number to the range of the array's index
   const randomIndex = Math.floor(Math.random() * quotes.length);
+  // Selects a random quote from the quotes array
   const randomQuote = quotes[randomIndex];
+  // Updates the text of the p element to the randomly selected quote
   quoteText.textContent = randomQuote;
 
-  // Change backgroun color
+  // document.body.style.backgroundColor = 'hsl(...)' changes background color of the page to a random hue
+  // hsl(${Math.random() * 360}, 100%, 90%) Genarates a random hue between 0 and 360
+  // The 100% below means full satuartion
+  // The 90% below means Lighness of 90% for a pastel-like color
   document.body.style.backgroundColor = `hsl(${
     Math.random() * 360
   }, 100%, 90%)`;
 
-  // Add animation
+  // Resets the animation
   quoteText.style.animation = "none";
+  // Re-applies the animation after a tiny delay to trigger it again.
   setTimeout(() => {
     quoteText.style.animation = "";
   }, 10);
